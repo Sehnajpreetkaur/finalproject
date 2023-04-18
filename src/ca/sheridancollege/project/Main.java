0ce651c65effdc9f8d72fd37352534bf0c70f8a1
sehnajpreet kaur <106169436+Sehnajpreetkaur@users.noreply.github.com>
2023-04-18 18:44:11 -0400

    Add files via upload

---
+++ src/ca/sheridancollege/project/Main.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author sehnajpreet kaur
+ */
+public class Main {
+    
+     public static void main(String[] args) throws Exception {
+        boolean exit = true;
+        Scanner input1 = new Scanner(System.in);
+        do {
+            try {
+                System.out.println("Please enter 1 to play BlackJack or 2 to exit");
+                int choice = input1.nextInt();
+                switch (choice) {
+                    case 1:
+                        BlackJack player = new BlackJack("BlackJack");
+                        player.play();
+                        break;
+                    case 2:
+                        exit = false;
+                        break;
+                    default:
+                        System.out.println("Please Enter 1 or 2");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid Input");
+                input1.next();
+            }
+        } while (exit);
+        input1.close();
+
+    }
+}
+    
+
